e8fb89f1632caaaef369b0831b4ab2bffb089ad5
josh <joshua2allen@gmail.com>
2024-01-04 21:21:08 -0600

    improved import transactions process

--- Angular-UI/src/app/components/import-transactions/import-transactions.component.ts
+++ Angular-UI/src/app/components/import-transactions/import-transactions.component.ts
@@ -9,27 +9,32 @@ import {
     MatDialogClose,
 } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { FileDropComponent } from '../file-drop/file-drop.component';
 import { MatTableModule } from '@angular/material/table';
 import Papa from 'papaparse';
-import { ParsedTransactionGrid, parseTransactions } from './transaction-parser';
+import { ParsedTransactionGrid, ParsedTransactionRow, parseTransactions } from './transaction-parser';
 import { TransactionService, TransactionToAdd } from '@services/transaction.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
     standalone: true,
-    imports: [CommonModule, FormsModule, MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatButtonModule, FileDropComponent, MatTableModule, MatSlideToggleModule],
+    imports: [CommonModule, FormsModule, MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, 
+        MatButtonModule, FileDropComponent, MatTableModule, MatSlideToggleModule, MatIconModule, MatCheckboxModule],
     templateUrl: './import-transactions.component.html'
 })
 export class ImportTransactionsComponent {
-    files: File[] | undefined;
+    files: File[] = [];
+    private importedFiles: File[] = [];
     fileColumns = ['file name', 'file type', 'file size'];
     fileIndex: number | undefined;
+    selection = new SelectionModel<ParsedTransactionRow>(true, []);
 
-    currentGridIndex: number = 0;
-    currentGrid: ParsedTransactionGrid | undefined;
-    grids: ParsedTransactionGrid[] | undefined;
+    grid: ParsedTransactionGrid | undefined;
+    currentFile: File | undefined;
     duplicateRowIndexes: number[] = [];
     invalidCount: number = 0;
     duplicateCount: number = 0;
@@ -44,8 +49,8 @@ export class ImportTransactionsComponent {
 
     }
 
-    processFiles(files: File[]) {
-        this.files = files;
+    addFiles(files: File[]) {
+        this.files.push(...files);
     }
     formatBytes(bytes: number, decimals: number) {
         if (bytes == 0) return '0 Bytes';
@@ -56,15 +61,10 @@ export class ImportTransactionsComponent {
         return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
     }
 
-    next() {
-        var parsedGrids: ParsedTransactionGrid[] = [];
-        var index = 0;
-        if (!this.files || !this.files.length) {
-            return;
-        }
+    beginImport(file: File){
+        this.currentFile = file;
         var config: Papa.ParseConfig = {
             complete: (results, f) => {
-                var file = this.files![index];
                 if (results.errors.length) {
                     alert(`Error CSV Parsing ${file.name}`)
                     return;
@@ -73,47 +73,67 @@ export class ImportTransactionsComponent {
                 if (!parsedGrid) {
                     return;
                 }
-                parsedGrids.push(parsedGrid);
-                index++;
-                if (index == this.files!.length) {
-                    this.showParsedGrids(parsedGrids);
-                } else {
-                    Papa.parse(<any>this.files![index], config)
-                }
+                this.showParsedGrid(parsedGrid);
             }
         };
-        Papa.parse(<any>this.files[0], config)
+        Papa.parse(<any>file, config)
     }
 
-    backToStart() {
-        this.files = undefined;
-        this.currentGrid = undefined;
-        this.grids = undefined;
+
+    back() {
+        this.grid = undefined;
+        this.currentFile = undefined;
+        this.invalidCount = 0;
+        this.duplicateCount = 0;
+        this.importCount = 0;
+        this.incomeCount = 0;
+        this.selection.clear();
     }
 
-    showParsedGrids(grids: ParsedTransactionGrid[]) {
-        this.currentGrid = grids[0];
-        this.currentGridIndex = 0;
-        this.grids = grids;
+    isImported(file: File): boolean{
+        return this.importedFiles.includes(file);
+    }
+
+    removeFile(file: File){
+        this.files = this.files!.filter(z => z != file);
+    }
+
+    getClass(row: ParsedTransactionRow, index: number, cell: any): string{
+        if (row.invalidIndexes.length){
+            return "text muted"
+        } else if (this.duplicateRowIndexes.includes(index)){
+            return "warning-cell";
+        } else if (typeof cell == "number"){
+            if ((this.expensesAreNegative && cell > 0) || (!this.expensesAreNegative && cell < 0)){
+                return "success-cell";
+            }
+        }
+        return "";
+    }
+
+    showParsedGrid(grid: ParsedTransactionGrid) {
+        this.grid = grid;
         this.duplicateRowIndexes = [];
         this.invalidCount = 0;
-        for (var i = 0; i < this.currentGrid.rows.length; i++) {
-            var row = this.currentGrid.rows[i];
+        for (var i = 0; i < grid.rows.length; i++) {
+            var row = grid.rows[i];
             if (row.invalidIndexes.length) {
                 this.invalidCount++;
                 continue;
             }
-            var date = <Date>row.cells[this.currentGrid.dateColumnIndex];
-            var amount = <number>row.cells[this.currentGrid.amountColumnIndex];
-            var name = <string>row.cells[this.currentGrid.nameColumnIndex];
+            var date = <Date>row.cells[grid.dateColumnIndex];
+            var amount = <number>row.cells[grid.amountColumnIndex];
+            var name = <string>row.cells[grid.nameColumnIndex];
             if (this.transactionService.isDuplicate(date, name, amount)) {
                 this.duplicateRowIndexes.push(i);
+            } else {
+                this.selection.select(row);
             }
         }
         this.duplicateCount = this.duplicateRowIndexes.length;
-        var rowsToImport = this.currentGrid.rows.filter((row, i) => !row.invalidIndexes.length && !this.duplicateRowIndexes.includes(i));
+        var rowsToImport = grid.rows.filter((row, i) => !row.invalidIndexes.length && !this.duplicateRowIndexes.includes(i));
         this.importCount = rowsToImport.length;
-        var negativeAmountCount = rowsToImport.filter(z => <any>z.cells[this.currentGrid!.amountColumnIndex] < 0).length;
+        var negativeAmountCount = rowsToImport.filter(z => <any>z.cells[grid!.amountColumnIndex] < 0).length;
         if (negativeAmountCount > this.importCount / 2) {
             this.expensesAreNegative = true; //since a majority of amounts are negative, auto-set this to true; 
         }
@@ -121,23 +141,23 @@ export class ImportTransactionsComponent {
     }
 
     expensesAreNegativeChanged() {
-        var rowsToImport = this.currentGrid!.rows.filter((row, i) => !row.invalidIndexes.length && !this.duplicateRowIndexes.includes(i));
-        var negativeAmountCount = rowsToImport.filter(z => <any>z.cells[this.currentGrid!.amountColumnIndex] < 0).length;
+        var rowsToImport = this.grid!.rows.filter((row, i) => !row.invalidIndexes.length && !this.duplicateRowIndexes.includes(i));
+        var negativeAmountCount = rowsToImport.filter(z => <any>z.cells[this.grid!.amountColumnIndex] < 0).length;
         this.incomeCount = this.expensesAreNegative ? (this.importCount - negativeAmountCount) : negativeAmountCount;
     }
 
     importTransactions() {
         var sign = this.expensesAreNegative ? -1 : 1;
-        var trxnsToAdd: TransactionToAdd[] = this.currentGrid!.rows.filter((z, i) => !z.invalidIndexes.length && !this.duplicateRowIndexes.includes(i)).map(z => {
+        var trxnsToAdd: TransactionToAdd[] = this.selection.selected.map(z => {
             var trxnToAdd: TransactionToAdd = {
-                name: <string>z.cells[this.currentGrid!.nameColumnIndex],
-                amount: <number>z.cells[this.currentGrid!.amountColumnIndex] * sign,
-                date: <Date>z.cells[this.currentGrid!.dateColumnIndex],
+                name: <string>z.cells[this.grid!.nameColumnIndex],
+                amount: <number>z.cells[this.grid!.amountColumnIndex] * sign,
+                date: <Date>z.cells[this.grid!.dateColumnIndex],
             }
-            if (this.currentGrid!.catNameColumnIndex != undefined && this.currentGrid!.subcatNameColumnIndex != undefined){
+            if (this.grid!.catNameColumnIndex != undefined && this.grid!.subcatNameColumnIndex != undefined){
                 var manualSubcategory = {
-                    catName: <string>z.cells[this.currentGrid!.catNameColumnIndex],
-                    subcatName: <string>z.cells[this.currentGrid!.subcatNameColumnIndex],
+                    catName: <string>z.cells[this.grid!.catNameColumnIndex],
+                    subcatName: <string>z.cells[this.grid!.subcatNameColumnIndex],
                 }
                 if (manualSubcategory.catName && manualSubcategory.subcatName){
                     trxnToAdd.manualSubcategory = manualSubcategory;
@@ -145,14 +165,10 @@ export class ImportTransactionsComponent {
             }
             return trxnToAdd;
         });
-        this.transactionService.addTransactions(trxnsToAdd, this.currentGrid!.filename)
+        this.transactionService.addTransactions(trxnsToAdd, this.grid!.filename)
         this.snackBar.open("Imported " + trxnsToAdd.length + " Transactions", "", { duration: 3000 });
-        if (this.currentGridIndex == this.grids!.length - 1) {
-            this.dialogRef.close();
-        } else {
-            this.currentGridIndex++;
-            this.currentGrid = this.grids![this.currentGridIndex];
-        }
+        this.importedFiles.push(this.currentFile!);
+        this.back();
     }
 
     isNumber(value: any): value is number {
