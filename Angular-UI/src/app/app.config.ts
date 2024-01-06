edc874221fe5706e16c28a09895876f5a6ffba9b
josh <joshua2allen@gmail.com>
2024-01-06 16:43:58 -0600

    added angular pwa

--- Angular-UI/src/app/app.config.ts
+++ Angular-UI/src/app/app.config.ts
@@ -1,9 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withHashLocation()), provideAnimations()]
+  providers: [provideRouter(routes, withHashLocation()), provideAnimations(), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })]
 };
